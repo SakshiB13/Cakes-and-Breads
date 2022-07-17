b6a7ebbe08453eca045aa7d276b291bbea635786
Sakshi Bole <98754992+SakshiB13@users.noreply.github.com>
2022-07-17 16:56:25 +0530

    Update MainSection.js

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -10,7 +10,7 @@ import '../App.css';
 function MainSection() {
  return (
     <div className='hero-image'>
-        <video  className="video-container" src='./videos/video1.mp4' autoPlay='autoplay' loop ='loop' muted='muted' />
+        <video src='./videos/video1.mp4' autoPlay='autoplay' loop ='loop' muted='muted' />
         <h1>CAKES & BREADS</h1>
         <p>Freshly baked with love!</p>
         <div className='hero-btns' style={{ justifyContent: "space-between" }}>
